756879c87ecf5bc3cf9027b4f09c827f4ecfc68c
Ofentse <fentsemosiane@gmail.com>
2025-10-19 15:19:41 +0200

    made migrations

--- ecommerce_api/urls.py
+++ ecommerce_api/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('products.urls')),
+    # Redirect root URL to the API landing
+    path('', RedirectView.as_view(url='/api/', permanent=False)),
 ]
